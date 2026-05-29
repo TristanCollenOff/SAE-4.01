cc1336bbfc7cc9f91f15bfff02a343df4a173cf5
TristanCollenOff <titicollen@gmail.com>
2026-05-29 11:10:28 +0200

    schema sql

--- app/schema.sql
+++ app/schema.sql
@@ -1,110 +1,113 @@
-DROP TABLE IF EXISTS Utilisateur;
-CREATE TABLE Utilisateur(
-   id_utilisateur INTEGER PRIMARY KEY AUTOINCREMENT,
-   nom_utilisateur VARCHAR(50) UNIQUE,
-   motdepasse VARCHAR(128),
-   role VARCHAR(50),
-   prenom VARCHAR(50),
-   nom VARCHAR(50),
-   age INTEGER,
-   email VARCHAR(100) UNIQUE,
-   login_attempts INTEGER DEFAULT 0,
-   block_until DATETIME,
-   last_login DATETIME
+DROP TABLE IF EXISTS affilier;
+DROP TABLE IF EXISTS Contenir;
+DROP TABLE IF EXISTS connexion;
+DROP TABLE IF EXISTS planification;
+DROP TABLE IF EXISTS fichier;
+DROP TABLE IF EXISTS lecteur;
+DROP TABLE IF EXISTS fichier_log;
+DROP TABLE IF EXISTS playlist;
+DROP TABLE IF EXISTS utilisateur;
+DROP TABLE IF EXISTS type_fichier;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS organisation;
+
+CREATE TABLE organisation(
+   id_organisation INTEGER PRIMARY KEY,
+   nom_organisation VARCHAR(50) UNIQUE NOT NULL
 );
 
-DROP TABLE IF EXISTS Organisation;
-CREATE TABLE Organisation(
-   id_organisation INTEGER PRIMARY KEY AUTOINCREMENT,
-   nom_organisation VARCHAR(50) UNIQUE
+CREATE TABLE role(
+   nom_role VARCHAR(50) PRIMARY KEY,
+   description TEXT
 );
 
-DROP TABLE IF EXISTS Playlist;
-CREATE TABLE Playlist(
-   id_playlist INTEGER PRIMARY KEY AUTOINCREMENT,
-   nom_playlist VARCHAR(50) UNIQUE,
-   date_creation DATETIME DEFAULT CURRENT_TIMESTAMP,
-   date_fin_playlist DATE,
-   date_derniere_maj DATETIME,
-   publie BOOLEAN DEFAULT 1
+CREATE TABLE type_fichier(
+   type_fichier VARCHAR(50) PRIMARY KEY
 );
 
-DROP TABLE IF EXISTS Fichier;
-CREATE TABLE Fichier(
-   id_fichier INTEGER PRIMARY KEY AUTOINCREMENT,
-   nom VARCHAR(100) NOT NULL,
-   emplacement VARCHAR(255),
-   duree_fichier INTEGER,
-   date_ajout DATETIME DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE utilisateur(
+   id_utilisateur INTEGER UNIQUE NOT NULL,
+   nom_utilisateur VARCHAR(50) UNIQUE NOT NULL,
+   motdepasse VARCHAR(50) NOT NULL,
+   prenom VARCHAR(50) NOT NULL,
+   nom VARCHAR(50) NOT NULL,
+   email VARCHAR(100) UNIQUE NOT NULL,
+   nom_role VARCHAR(50) NOT NULL,
+   FOREIGN KEY(nom_role) REFERENCES role(nom_role)
 );
 
-DROP TABLE IF EXISTS Lecteur;
-CREATE TABLE Lecteur(
-   id_lecteur INTEGER PRIMARY KEY AUTOINCREMENT,
-   nom_lecteur VARCHAR(50) NOT NULL UNIQUE,
-   adresseIP VARCHAR(50) UNIQUE,
-   etat_lecteur VARCHAR(50) DEFAULT 'Hors ligne',
-   emplacement VARCHAR(50),
-   derniere_synchro DATETIME,
-   adresse_lecteur VARCHAR(50),
+CREATE TABLE playlist(
+   id_playlist INTEGER PRIMARY KEY,
+   nom_playlist VARCHAR(50) UNIQUE NOT NULL,
+   date_creation DATE NOT NULL,
+   date_fin_playlist DATE NOT NULL,
+   date_derniere_maj DATE NOT NULL,
+   publie INTEGER NOT NULL,
    id_organisation INTEGER NOT NULL,
-   playlist_active_id INTEGER, 
-   volume INTEGER DEFAULT 80,
-   alerte_active BOOLEAN DEFAULT 0,
-   FOREIGN KEY(id_organisation) REFERENCES Organisation(id_organisation),
-   FOREIGN KEY(playlist_active_id) REFERENCES Playlist(id_playlist)
+   FOREIGN KEY(id_organisation) REFERENCES organisation(id_organisation)
 );
 
-DROP TABLE IF EXISTS Planification;
-CREATE TABLE Planification(
-   id_planification INTEGER PRIMARY KEY AUTOINCREMENT,
-   id_lecteur INTEGER NOT NULL,
-   id_playlist INTEGER NOT NULL,
-   jour_semaine VARCHAR(10), 
-   heure_debut TIME,
-   heure_fin TIME,
-   date_specifique DATE, 
-   FOREIGN KEY(id_lecteur) REFERENCES Lecteur(id_lecteur),
-   FOREIGN KEY(id_playlist) REFERENCES Playlist(id_playlist)
+CREATE TABLE planification(
+   id_planification INTEGER PRIMARY KEY,
+   heure_debut TIME NOT NULL,
+   heure_fin TIME NOT NULL,
+   date_ DATE NOT NULL,
+   id_playlist INT NOT NULL,
+   FOREIGN KEY(id_playlist) REFERENCES playlist(id_playlist)
 );
 
-DROP TABLE IF EXISTS FichierLog;
-CREATE TABLE FichierLog (
-    id_log INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER,
-    username VARCHAR(50), 
-    type_log VARCHAR(50), 
-    message VARCHAR(255),
-    date_fichierlog DATETIME DEFAULT (datetime('now', 'localtime')),
-    FOREIGN KEY(user_id) REFERENCES Utilisateur(id_utilisateur)
+CREATE TABLE fichier_log(
+   id_log INTEGER PRIMARY KEY,
+   type_action VARCHAR(50) NOT NULL,
+   message VARCHAR(50) NOT NULL,
+   date_fichierlog DATE NOT NULL,
+   id_organisation INT NOT NULL,
+   FOREIGN KEY(id_organisation) REFERENCES organisation(id_organisation)
 );
 
-DROP TABLE IF EXISTS Conçoit_une;
-CREATE TABLE Conçoit_une(
-   id_utilisateur INT,
-   id_playlist INT,
-   PRIMARY KEY(id_utilisateur, id_playlist),
-   FOREIGN KEY(id_utilisateur) REFERENCES Utilisateur(id_utilisateur),
-   FOREIGN KEY(id_playlist) REFERENCES Playlist(id_playlist)
+CREATE TABLE lecteur(
+   id_lecteur INTEGER PRIMARY KEY,
+   nom_lecteur VARCHAR(50) UNIQUE NOT NULL,
+   adresseIP VARCHAR(50) UNIQUE NOT NULL,
+   etat_lecteur VARCHAR(50) NOT NULL,
+   emplacement VARCHAR(50) NOT NULL,
+   derniere_synchro DATE NOT NULL,
+   adresse_lecteur VARCHAR(50) NOT NULL,
+   alerte INTEGER NOT NULL,
+   id_organisation INTEGER NOT NULL,
+   FOREIGN KEY(id_organisation) REFERENCES organisation(id_organisation)
 );
 
-DROP TABLE IF EXISTS Est_composé_d_une;
-CREATE TABLE Est_composé_d_une(
-   id_playlist INT,
-   id_fichier INT,
+CREATE TABLE fichier(
+   id_fichier INTEGER PRIMARY KEY,
+   nom VARCHAR(50) UNIQUE NOT NULL,
+   chemin VARCHAR(50) NOT NULL,
+   duree_fichier INTEGER NOT NULL,
+   date_maj DATE NOT NULL,
+   type_fichier VARCHAR(50) NOT NULL,
+   FOREIGN KEY(type_fichier) REFERENCES type_fichier(type_fichier)
+);
+
+CREATE TABLE connexion(
+   id_utilisateur INTEGER PRIMARY KEY,
+   login_attempts INTEGER,
+   block_until DATE,
+   last_login DATE,
+   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur)
+);
+
+CREATE TABLE Contenir(
+   id_playlist INTEGER,
+   id_fichier INTEGER,
    PRIMARY KEY(id_playlist, id_fichier),
-   FOREIGN KEY(id_playlist) REFERENCES Playlist(id_playlist),
-   FOREIGN KEY(id_fichier) REFERENCES Fichier(id_fichier)
+   FOREIGN KEY(id_playlist) REFERENCES playlist(id_playlist),
+   FOREIGN KEY(id_fichier) REFERENCES fichier(id_fichier)
 );
 
-DROP TABLE IF EXISTS planifie_une;)
-);
-
-DROP TABLE IF EXISTS Travaille_ensemble;
-CREATE TABLE Travaille_ensemble(
-   id_utilisateur INT,
-   id_organisation INT,
+CREATE TABLE affilier(
+   id_utilisateur INTEGER,
+   id_organisation INTEGER,
    PRIMARY KEY(id_utilisateur, id_organisation),
-   FOREIGN KEY(id_utilisateur) REFERENCES Utilisateur(id_utilisateur),
-   FOREIGN KEY(id_organisation) REFERENCES Organisation(id_organisation)
+   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur),
+   FOREIGN KEY(id_organisation) REFERENCES organisation(id_organisation)
 );
