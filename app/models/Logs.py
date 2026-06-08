81e615dd646aa87ed32fd47be77f9be9755f0e8a
BADEDJI Antoine <12402371@s30614.iutv.univ-paris13.fr>
2026-06-08 15:47:25 +0200

    Modif Logs.py

--- app/models/Logs.py
+++ app/models/Logs.py
@@ -1,34 +1,32 @@
 from datetime import datetime
 
 class Logs:   
-    def __init__(self, id_fichierlog=None, user_id=None, username=None, type_log=None, message=None, date_fichierlog=None):
-        self.id_fichierlog = id_fichierlog
-        self.user_id = user_id
-        self.username = username
-        self.type_log = type_log
+    def __init__(self, id_log=None, type_action=None, message=None, date_fichierlog=None, id_organisation=None):
+        self.id_log = id_log
+        self.type_action = type_action
         self.message = message
         self.date_fichierlog = date_fichierlog if date_fichierlog else datetime.now()
+        self.id_organisation = id_organisation
     
     def to_dict(self):
+        
         return {
-            'id_fichierlog': self.id_fichierlog,
-            'user_id': self.user_id,
-            'username': self.username,
-            'type_log': self.type_log,
+            'id_log': self.id_log,
+            'type_action': self.type_action,
             'message': self.message,
-            'date_fichierlog': self.date_fichierlog
+            'date_fichierlog': self.date_fichierlog,
+            'id_organisation': self.id_organisation
         }
     
     @staticmethod
     def from_dict(data):
         return Logs(
-            id_fichierlog=data.get('id_fichierlog'),
-            user_id=data.get('user_id'),
-            username=data.get('username'),
-            type_log=data.get('type_log'),
+            id_log=data.get('id_log'),
+            type_action=data.get('type_action'),
             message=data.get('message'),
-            date_fichierlog=data.get('date_fichierlog')
+            date_fichierlog=data.get('date_fichierlog'),
+            id_organisation=data.get('id_organisation')
         )
     
     def __repr__(self):
-        return f"<Logs {self.id_fichierlog} - {self.username} - {self.type_log}>"
+        return f"<Logs {self.id_log} - Org {self.id_organisation} - {self.type_action}>"
